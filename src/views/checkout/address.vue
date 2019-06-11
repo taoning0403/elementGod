2c4de7656f920e96ddb074977a75affe1fce6459
xiaozhipeng <18296970472@163.com>
2019-06-11 21:55:14 +0800

    确认订单页面地址选择

---
+++ src/views/checkout/address.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+    <div class="header">
+      <router-link to="/checkout/checkout">
+        <img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyMCIgaGVpZ2h0PSIzMiI+PHBhdGggZmlsbD0iI2ZmZiIgZD0iTTE2LjU1MiA1LjYzM0wxNC41MDggMy41OSAyLjI0MyAxNS44NTMgMTQuNTA4IDI4LjQxbDIuMDQ0LTIuMDQzLTEwLjIyLTEwLjUxM3oiLz48L3N2Zz4=">
+      </router-link>
+      <h1>添加地址</h1>
+    </div>
+    <van-address-edit
+    :area-list="areaList"
+    show-set-default
+    @save="onSave"
+
+  />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      areaList: {
+        province_list: {
+          110000: '北京市',
+          120000: '深圳市'
+        },
+        city_list: {
+          110100: '北京市',
+          110200: '县',
+          120100: '深圳市',
+          120200: '县'
+        },
+        county_list: {
+          110101: '东城区',
+          110102: '西城区',
+          110105: '朝阳区',
+          110106: '丰台区',
+          120101: '宝安区',
+          120102: '河东区',
+          120103: '河西区',
+          120104: '南开区',
+          120105: '河北区',
+        }
+      },
+    }
+  },
+  methods: {
+    onSave() {
+      window.location.href = 'http://localhost:8082/#/checkout/checkout'
+    },
+
+  }
+}
+</script>
+
+<style lang="less">
+.header{
+  height: 44px;
+  background-image: linear-gradient(90deg,#0af,#0085ff);
+  display: flex;
+  img {
+    margin-top: 15px;
+    margin-left: 10px;
+    width: 15px;
+    height: 15px;
+  }
+  h1{
+    flex: 1;
+    margin-top: 15px;
+    text-align: center;
+    font-size: 16px;
+    color: #fff;
+    font-weight: 900;
+  }
+}
+</style>
+
---
+++ src/views/checkout/checkout.vue
@@ -0,0 +1,97 @@
+<template>
+  <div>
+    <div class="header">
+      <router-link to="/shop">
+        <img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyMCIgaGVpZ2h0PSIzMiI+PHBhdGggZmlsbD0iI2ZmZiIgZD0iTTE2LjU1MiA1LjYzM0wxNC41MDggMy41OSAyLjI0MyAxNS44NTMgMTQuNTA4IDI4LjQxbDIuMDQ0LTIuMDQzLTEwLjIyLTEwLjUxM3oiLz48L3N2Zz4=">
+      </router-link>
+      <h1>确认订单</h1>
+    </div>
+
+    <div class="address">
+      <p>订单配送至</p>
+      <router-link class="address-choose" to="/checkout/address">
+        新增收货地址 >
+      </router-link>
+    </div>
+
+    <div class="checkout-keycard">
+      <div class="keycard">
+        <div class="delivery">
+          <strong>送达时间</strong>
+          <span>尽快送达</span>
+        </div>
+        <div class="card-item">
+          <strong>支付方式</strong>
+          <span>在线支付</span>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+@import '~@/styles/common/mixins.less';
+  .header{
+    height: 44px;
+    background-image: linear-gradient(90deg,#0af,#0085ff);
+    display: flex;
+    img {
+      margin-top: 15px;
+      margin-left: 10px;
+      width: 15px;
+      height: 15px;
+    }
+    h1{
+      flex: 1;
+      margin-top: 15px;
+      text-align: center;
+      font-size: 16px;
+      color: #fff;
+      font-weight: 900;
+    }
+  }
+  .address {
+    height: 85px;
+    background-image: linear-gradient(90deg,#0af,#0085ff);
+    padding: 16px 10px 11px;
+    box-sizing: border-box;
+    p {
+      color:hsla(0,0%,100%,.8);
+      font-size: 14px;
+      margin-bottom: 10px;
+    }
+    .address-choose {
+      font-weight: 900;
+      font-size: 24px;
+      color:hsla(0,0%,100%,.8);
+    }
+  }
+  .checkout-keycard {
+    background-image: linear-gradient(90deg,#0af,#0085ff);
+    height: 105px;
+    padding: 0 5px;
+    .keycard {
+      height: 100px;
+      background: #fff;
+      padding: 0 20px;
+      font-size: 14px;
+      .delivery, .card-item {
+        .border-bottom;
+        line-height: 50px;
+      }
+       span {
+        color: blue;
+        position: absolute;
+        right: 0;
+      }
+
+    }
+  }
+</style>
