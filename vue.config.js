07db19e951219261e039f167c28b6217c44abdd6
Duchil <duchin.leong@aliyun.com>
2019-06-12 01:05:48 +0800

    discover顶部

--- vue.config.js
+++ vue.config.js
@@ -11,6 +11,7 @@ module.exports = {
       // key - 请求前缀
       // value - 代理选项设置
       // http://localhost:8080/maoyan/api/find => http://m.maoyan.com/api/find
+      // pathRewrite 路径重写 key:重写的路径名称 value设置为''
       '/maoyan': {
         target: 'http://m.maoyan.com/',
         pathRewrite: {
