8a842aa2de7e2d9aa98ed99d38ec24068d01f0de
xiaozhipeng <18296970472@163.com>
2019-06-14 08:34:20 +0800

    确认订单页面商品订单模块与订单页面

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from './router/index'
 import store from './store'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-import { Swipe, SwipeItem, AddressEdit, Toast } from 'vant'
+import { Swipe, SwipeItem, AddressEdit, Toast, Actionsheet } from 'vant'
 
 import './styles/base.less'
 
@@ -13,6 +13,7 @@ Vue.use(ElementUI)
   .use(SwipeItem)
   .use(AddressEdit)
   .use(Toast)
+  .use(Actionsheet)
 
 Vue.config.productionTip = false
 
